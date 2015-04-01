7e820051662161de23292deaa71cd58fc2fe68aa
Nahuel Defosse <nahuel.defosse@gmail.com>
2015-04-01 15:57:43 -0300

    Some ideas checking out rx Subject

---
+++ dsl.py
@@ -0,0 +1,44 @@
+from rx.subject import BehaviorSubject
+
+
+class ValueStore(object):
+    def get_inital_value(self, name):
+        """
+        DB lookups, etc.
+        """
+        raise NotImplementedError("Error")
+
+
+class ValueStoreDjango(object):
+
+    def get_inital_value(self, name):
+        if 'DI' in name.upper():
+            return 0
+        elif 'AI' in name.upper():
+            return 0
+        else:
+            return '0'
+
+
+class ValueCollection(object):
+    def __init__(self, value_store=None):
+        self.values = {}
+        self.value_store = value_store
+
+    def get(self, name):
+        if name not in self.values:
+            self.values[name] = self.create(name)
+        return self.values[name]
+
+    def create(self, name):
+        initial = self.value_store.get_inital_value(name)
+        subject = BehaviorSubject(initial)
+        return subject
+
+
+class SVGDSL(object):
+
+    def __init__(self):
+        self.values = ValueCollection()
+        self.formulas = []
+
